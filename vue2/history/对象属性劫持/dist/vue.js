ff4d7b87a671ee6a34fe8c51487480e08408be84
fengyayun <fengyayun@941949529@qq.com>
2022-08-29 13:49:31 +0800

    feat:  实现对数组方法对劫持

--- vue2/history/对象属性劫持/dist/vue.js
+++ vue2/history/对象属性劫持/dist/vue.js
@@ -39,55 +39,11 @@
     return Constructor;
   }
 
-  var oldArrayProto = Array.prototype;
-  var arrayMethods = Object.create(oldArrayProto);
-  var methods = ['push', 'pop', 'shift', 'unshift', 'sort', 'reverse', 'splice'];
-  methods.forEach(function (method) {
-    arrayMethods[method] = function () {
-      var ob = this.__ob__;
-      var inserted; //有些新增会增加一些对象 要对这些对象重新定义属性
-
-      for (var _len = arguments.length, args = new Array(_len), _key = 0; _key < _len; _key++) {
-        args[_key] = arguments[_key];
-      }
-
-      switch (method) {
-        case 'push':
-        case 'unshift':
-          inserted = args;
-          break;
-
-        case 'splice':
-          inserted = args.slice(2);
-          break;
-      } //对于新增的数据要想实现重新定义属性必须拿observe.observeArray
-
-
-      if (inserted && inserted.length > 0) ob.observeArray(inserted);
-      var result = oldArrayProto[method].apply(this, args);
-      return result;
-    };
-  });
-
   var Observe = /*#__PURE__*/function () {
     function Observe(data) {
       _classCallCheck(this, Observe);
 
-      //被观测过的对象上加一个__ob__属性并且不让被遍历到
-      Object.defineProperty(data, '__ob__', {
-        value: this,
-        enumerable: false,
-        configurable: false
-      }); //对于数组默认观测的是索引 数组较大会非常耗性能 而且开发过程该数组索引的场景也是非常低 需要对数组常用的方法进行拦截
-      //对于数组的长度以及索引改变没有被监测
-
-      if (Array.isArray(data)) {
-        //考虑兼容性的话可以遍历赋值
-        data.__proto__ = arrayMethods;
-        this.observeArray(data);
-      } else {
-        this.walk(data);
-      }
+      this.walk(data);
     }
 
     _createClass(Observe, [{
@@ -95,14 +51,6 @@
       value: function walk(data) {
         Object.keys(data).forEach(function (key) {
           return defineReactive(data, key, data[key]);
-        });
-      }
-    }, {
-      key: "observeArray",
-      value: function observeArray(data) {
-        if (!Array.isArray(data)) return;
-        data.forEach(function (item) {
-          return observe(item);
         });
       }
     }]);
@@ -121,6 +69,7 @@
         value = newValue;
       },
       get: function get() {
+        console.log('获取值');
         return value;
       }
     });
@@ -129,11 +78,6 @@
   function observe(data) {
     //只监测对象类型 
     if (!(data && _typeof(data) === 'object')) return;
-
-    if (data.__ob__) {
-      return data;
-    }
-
     return new Observe(data);
   }
 
