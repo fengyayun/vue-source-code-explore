0f27a1b4a3bc7f6a7e2f0282876e6952e4c8f0e6
冯亚运 <fengyayun@100.me>
2022-09-08 13:53:41 +0800

    feat:vue 完整版本diff算法实现 由于v-for 指令暂无实现所以可以采用 index.js中注视的代码进行测试diff

---
+++ vue2/history/16.完整diff的实现/src/init.js
@@ -0,0 +1,40 @@
+import { initState } from "./state"
+import { compilerToFunctions } from "./compiler"
+import { callHook, mountComponent } from "./lifeCycle"
+import { mergeOptions } from "./utils"
+
+export function initMixin(Vue){
+  Vue.prototype._init = function(options){
+    const vm = this
+    // vue实例属性都会加一个$开头来区分用户的变量  合并全局上的options
+    vm.$options = mergeOptions(vm.constructor.options,options)
+
+    callHook(vm,'beforeCreate')
+    //初始化状态
+    initState(vm)
+
+    callHook(vm,'created')
+
+    if (vm.$options.el){
+      //挂载逻辑都聚合在$mount中 因为有时候用户可以不传el而是直接调用$mount
+      this.$mount(vm.$options.el)
+    }
+
+  }
+  Vue.prototype.$mount = function(el){
+    const vm = this
+    const options = vm.$options
+    //执行的优先级是 render => options中的template => 模版中的template
+    el = document.querySelector(el)
+    if (!options.render){
+      let template = options.template
+      if (!template){
+        //有兼容问题暂不考虑
+        template = el.outerHTML
+      }
+      const render = compilerToFunctions(template)
+      options.render = render
+    }
+    mountComponent(vm,el)
+  }
+}
