2af5a409de7d6f94e9497f1e38504b1f3132f239
冯亚运 <fengyayun@100.me>
2022-09-15 09:53:37 +0800

    feat:vue3 demo初始化

---
+++ vue3_demo/src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    <h1>111</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+  components: {
+  }
+}
+</script>
